0e613d73689a15dd79144575b7e70fff82329a91
ganzgagah6-netizen <ganzgagah6@gmail.com>
2026-09-16 17:53:57 +0800

    Create ClickerService.kt

---
+++ app/src/main/java/com/example/autoclicker/ClickerService.kt
@@ -0,0 +1,32 @@
+package com.example.autoclicker
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.view.accessibility.AccessibilityEvent
+
+class ClickerService : AccessibilityService() {
+
+    companion object {
+        var instance: ClickerService? = null
+    }
+
+    override fun onServiceConnected() {
+        instance = this
+    }
+
+    fun clickAt(x: Float, y: Float) {
+        val path = Path().apply { moveTo(x, y) }
+        val stroke = GestureDescription.StrokeDescription(path, 0, 50)
+        val gesture = GestureDescription.Builder().addStroke(stroke).build()
+        dispatchGesture(gesture, null, null)
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    override fun onInterrupt() {}
+
+    override fun onDestroy() {
+        instance = null
+        super.onDestroy()
+    }
+}
